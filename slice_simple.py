b407f3bf34f06ae5d18d696e95651663bf2b9f06
amielpoletti <162723122+amielpoletti@users.noreply.github.com>
2024-04-11 12:27:54 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,8 +1,12 @@
 def slice_simple():
     texto = "Awesome"
-    # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
-    # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
-
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
+    def slice_simple():
+    texto = "Awesome"
+    tres_iniciales = texto[:3].lower()
+    print(tres_iniciales)
+    CaracterNMedio_txt = len(texto) // 2
+    medio_stingtxt = texto[CaracterNMedio_txt-1:CaracterNMedio_txt+2]
+    print(medio_stingtxt)
+    CaracterTot=len(texto)
+    print(texto[:4].lower() +texto[CaracterTot-3:].lower())
+    
